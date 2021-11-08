a8052a7486aaea891a3b3614ad3c99b1511cf3e7
Gareth <gareth.helmer@gmail.com>
2021-11-08 07:55:59 +0000

    add docs to RouteNode

--- lib/protocol/route_node.dart
+++ lib/protocol/route_node.dart
@@ -1,5 +1,8 @@
 part of 'protocol.dart';
 
+///Class to abscarct all infomation about a node in a message route
+///
+///[isSender] and [isReceiver] can't be true at the same time.
 class RouteNode {
   RouteNode({
     required this.deviceId,
@@ -10,7 +13,12 @@ class RouteNode {
           'isSender and isReciver can not be true at the same time',
         );
 
+  ///Id to identify the device
   String deviceId;
+
+  ///Boolean value wether [RouteNode] is sender or not
   bool isSender;
+
+  ///Boolean value wether [RouteNode] is receiver or not
   bool isReceiver;
 }
