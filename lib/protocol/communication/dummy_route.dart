e2783d5bc435ba0701596125f61ed5a7338c6f4f
Hgamo <helmer.developer@gmail.com>
2021-12-30 14:15:07 +0000

    fix dummy route

--- lib/protocol/communication/dummy_route.dart
+++ lib/protocol/communication/dummy_route.dart
@@ -4,8 +4,8 @@ part of 'communication.dart';
 
 MessageRoute getDummyRoute() {
   return [
-    RouteNode(deviceId: '1', isSender: false, isReceiver: false),
+    RouteNode(deviceId: '1', isSender: true, isReceiver: false),
     RouteNode(deviceId: '2', isSender: false, isReceiver: false),
-    RouteNode(deviceId: '3', isSender: false, isReceiver: false),
+    RouteNode(deviceId: '3', isSender: false, isReceiver: true),
   ];
 }
