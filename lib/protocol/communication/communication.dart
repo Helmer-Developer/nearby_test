6e53076e0bc0390c1bff7eb7b338f1a1f45d9265
Hgamo <helmer.developer@gmail.com>
2021-12-30 14:14:52 +0000

    integarte into one file

--- lib/protocol/communication/communication.dart
+++ lib/protocol/communication/communication.dart
@@ -1,21 +1,50 @@
 library communication;
 
-import 'dart:typed_data';
-
 import 'package:nearby_test/global/globals.dart';
 import 'package:nearby_test/protocol/protocol.dart';
-import 'package:nearby_connections/nearby_connections.dart';
+import 'package:nearby_test/protocol/communication/ncp_service/ncp_service.dart';
 import 'package:uuid/uuid.dart';
 
-part 'send_neighbors.dart';
 part 'dummy_route.dart';
-part 'handle_message.dart';
 
 class Communications {
-  static Function sendNeighborsToId = comSendNeighborsToId;
+  Communications(this.ncpService);
 
-  static sendMessageToId(Message message, String id) {
-    const String data = 'dummy message'; //TODO: Add to Json implementation
-    Nearby().sendBytesPayload(id, Uint8List.fromList(data.codeUnits));
+  final NcpService ncpService;
+
+  void sendNeighborsToId({
+    required String receiverId,
+    required String senderId,
+    required List<DiscoverDevice> devices,
+    required MessageRoute route,
+  }) {
+    final List<Map> data = devices.map((device) => device.toMap()).toList();
+    final Message message = Message(
+      id: const Uuid().v4(),
+      senderId: senderId,
+      receiverId: receiverId,
+      route: route,
+      payload: data,
+      messageType: MessageType.neighborsResponse,
+    );
+  }
+
+  ///Handles and incomming message
+  ///
+  ///Needs [ownId] to put message in context to device
+  ///Retruns the Messsge if device is last node in messageroute
+  Message? handleMessage(Message message, String ownId) {
+    if (message.receiverId == ownId) {
+      return message;
+    }
+    final MessageRoute messageRoute = message.route;
+    final List<String> routeDeviceIdList =
+        messageRoute.map<String>((node) => node.deviceId).toList();
+    final ownIndex = routeDeviceIdList.indexOf(ownId);
+    sendMessageToId(message, routeDeviceIdList[ownIndex + 1]);
+  }
+
+  sendMessageToId(Message message, String id) {
+    ncpService.sendMessageToId(message, id);
   }
 }
