d5ce528d28da315bbc532f1253b2bddfae0a6415
Hgamo <helmer.developer@gmail.com>
2021-12-30 22:23:21 +0000

    add tests

---
+++ test/communication_test.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nearby_test/protocol/communication/communication.dart';
+import 'package:nearby_test/protocol/protocol.dart';
+import 'package:uuid/uuid.dart';
+
+import 'dummy_ncp_service.dart';
+
+main() {
+  group('Communication library test ', () {
+    final Communications communications = Communications(DummyNcpService());
+    final message = Message(
+      id: const Uuid().v4(),
+      senderId: '1',
+      receiverId: '2',
+      route: [
+        RouteNode(deviceId: '1', isSender: true, isReceiver: false),
+        RouteNode(deviceId: '2', isSender: false, isReceiver: true),
+      ],
+      payload: 'test',
+      messageType: MessageType.text,
+    );
+    test('basic send message test', () {
+      try {
+        communications.sendMessageToId(
+          message,
+          '2',
+        );
+      } catch (e) {
+        expect(
+          e,
+          null,
+          reason: 'should not throw exception',
+        );
+      }
+      expect(
+        message.runtimeType,
+        Message,
+        reason: 'should be Message',
+      );
+    });
+    test('HandleMessage test', () {
+      final possibleMessage = communications.handleMessage(message, '2');
+      expect(
+        possibleMessage,
+        message,
+        reason: 'should return the same message because own id is receiver',
+      );
+      final possibleMessage2 = communications.handleMessage(message, '1');
+      expect(
+        possibleMessage2,
+        null,
+        reason: 'should return null because own id is sender',
+      );
+    });
+  });
+}
---
+++ test/dummy_ncp_service.dart
@@ -0,0 +1,13 @@
+import 'package:nearby_test/protocol/communication/ncp_service/ncp_service.dart';
+import 'package:nearby_test/protocol/protocol.dart';
+
+class DummyNcpService with NcpService {
+  @override
+  void sendMessageToId(Message message, String id) {
+    print('''
+    Would send:
+    Message: $message
+    To: $id
+    ''');
+  }
+}
